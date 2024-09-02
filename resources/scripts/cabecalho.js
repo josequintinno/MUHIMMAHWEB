37865070d2298e1963b5cc5c2c09ea6253d40a95
José Quintinno <josequintinno@icloud.com>
2024-09-01 22:13:41 -0300

    🔵 feature: Implementar Modal de Configuração de Perfil de Usuário

--- resources/scripts/cabecalho.js
+++ resources/scripts/cabecalho.js
@@ -1,6 +1,14 @@
 const usuarioConfiguracaoView = document.querySelector(".usuario-configuracao");
 const usuarioView = document.querySelector(".usuario");
 
+const perfilUsuarioLinkView = document.getElementById("perfil-usuario-link");
+const usuarioConfiguracaoPerfilView = document.querySelector(".usuario-configuracao-perfil");
+
 usuarioView.addEventListener("click", function() {
     usuarioConfiguracaoView.classList.toggle("apresentar");
+});
+
+perfilUsuarioLinkView.addEventListener("click", function() {
+    usuarioConfiguracaoView.classList.remove("apresentar");
+    usuarioConfiguracaoPerfilView.classList.toggle("apresentar-modal-perfil");    
 });
