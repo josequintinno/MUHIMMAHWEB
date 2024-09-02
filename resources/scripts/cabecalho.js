f4ecc483dea3c46271f5d54f97f086f7933444b1
José Quintinno <josequintinno@icloud.com>
2024-09-01 22:52:28 -0300

    🔵 feature: Implementar fechamento de modal

--- resources/scripts/cabecalho.js
+++ resources/scripts/cabecalho.js
@@ -4,6 +4,8 @@ const usuarioView = document.querySelector(".usuario");
 const perfilUsuarioLinkView = document.getElementById("perfil-usuario-link");
 const usuarioConfiguracaoPerfilView = document.querySelector(".usuario-configuracao-perfil");
 
+const modalBotaoCancelarView = document.getElementById("modal-botao-cancelar");
+
 usuarioView.addEventListener("click", function() {
     usuarioConfiguracaoView.classList.toggle("apresentar");
 });
@@ -11,4 +13,8 @@ usuarioView.addEventListener("click", function() {
 perfilUsuarioLinkView.addEventListener("click", function() {
     usuarioConfiguracaoView.classList.remove("apresentar");
     usuarioConfiguracaoPerfilView.classList.toggle("apresentar-modal-perfil");    
+});
+
+modalBotaoCancelarView.addEventListener("click", () => {
+    usuarioConfiguracaoPerfilView.classList.remove("apresentar-modal-perfil");
 });
